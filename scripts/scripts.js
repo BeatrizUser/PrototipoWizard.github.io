b4f821b956a3e032bd7d6ba288b16b908a799a3e
BeatrizUser <beatriz.machadosilva@gmail.com>
2022-10-22 08:37:28 -0300

    modificacoes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,31 +1,28 @@
 $(document).ready(function(){
+var bg = document.querySelector(".bg-screen");
+var char1 = document.querySelector(".silueta1-screen");
+var char2 = document.querySelector(".silueta2-screen");
     $(function () {
         // FUNDOS
         $("#bg1").click(function(){
-            var img = document.querySelector(".bg-screen");
-            img.setAttribute('src', 'assets/bg1.png');
+            bg.setAttribute('src', 'assets/bg1.png');
         })      
         $("#bg2").click(function(){
-            var img = document.querySelector(".bg-screen");
-            img.setAttribute('src', 'assets/bg2.png');
+            bg.setAttribute('src', 'assets/bg2.png');
         })
         // PERSONAGENS 1
         $("#char1-1").click(function(){
-            var img = document.querySelector(".silueta1-screen");
-            img.setAttribute('src', 'assets/siluetahomem1.png');
+            char1.setAttribute('src', 'assets/siluetahomem1.png');
         })      
         $("#char1-2").click(function(){
-            var img = document.querySelector(".silueta1-screen");
-            img.setAttribute('src', 'assets/siluetahomem2.png');
+            char1.setAttribute('src', 'assets/siluetahomem2.png');           
         })
         // PERSONAGENS 2
         $("#char2-1").click(function(){
-            var img = document.querySelector(".silueta2-screen");
-            img.setAttribute('src', 'assets/siluetamulher1.png');
+            char2.setAttribute('src', 'assets/siluetamulher1.png');      
         })      
         $("#char2-2").click(function(){
-            var img = document.querySelector(".silueta2-screen");
-            img.setAttribute('src', 'assets/siluetamulher2.png');
+            char2.setAttribute('src', 'assets/siluetamulher2.png');        
         })
     });
 })
